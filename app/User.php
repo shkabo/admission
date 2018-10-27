dca5dd5067f049bab88c28746d9e8a774c6b8748
Bosko Stupar <bosko.stupar@gmail.com>
2018-10-27 16:56:22 +0200

    modified login - if user is active or not

--- app/User.php
+++ app/User.php
@@ -15,9 +15,10 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array
-     *
+     */
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'active',
     ];
 
     /**
